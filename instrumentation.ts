530541c7dabc268a364368ac4266d0dd6f7654fc
Semyon Shapoval <sem778shapoval@gmail.com>
2026-07-22 11:26:50 +0700

    fix: reinstall packages

--- instrumentation.ts
+++
@@ -1,7 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    const { getOplConfig } =
-      await import("./features/ory-admin/utils/oplRuntimeConfig")
-    getOplConfig()
-  }
-}
